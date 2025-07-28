9a3a841bc626486b5747c621a4bb4bb126af29b9
yong-king <1084948317@qq.com>
2025-07-28 16:32:49 +0800

    docker-compose can run. next is k8s

--- job-service/start.sh
+++ job-service/start.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
-/app/wait-for-it.sh mysql 3306 -t 60
-/app/wait-for-it.sh canal 11111 -t 60
-/app/wait-for-it.sh kafka 19092 -t 60
-/app/wait-for-it.sh elasticsearch 9200 -t 60
+/app/wait-for-it.sh kafka 19092
+/app/wait-for-it.sh elasticsearch 9200
+
 
 echo "All dependencies are up. Starting job-service..."
 exec ./job-service -conf /app/configs/config_doc.yaml
